06feedecd4f41a21296974baee10c57284f13da5
Yevhenii Kolchyba <kolchiba777@gmail.com>
2021-02-12 12:12:41 +0200

    version #2

--- src/main/java/com/foodDelivery/project/food/FoodDAO.java
+++
@@ -1,9 +0,0 @@
-package com.foodDelivery.project.food;
-
-import java.util.List;
-
-public interface FoodDAO {
-    // Interface for saving / retrieving an entity from the database
-    List<Food> findAll();
-
-   }
